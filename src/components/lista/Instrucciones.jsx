ece781e3bfd8d22cc3e5d17f46eb9f44ef47ed92
JhonatanCremona <jfulguera@creminox.com>
2023-04-25 20:19:41 -0300

    Agregando: texto-instrucciones del funcionamiento de los iconos en el component Instrucciones.jsx

--- src/components/lista/Instrucciones.jsx
+++ src/components/lista/Instrucciones.jsx
@@ -7,19 +7,19 @@ export const Instrucciones = () => {
             id:1,
             pathD:"M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z",
             spanText: "Editar etiqueta",
-            textDet:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor in delectus fugit dolores quod"
+            textDet:"Proximamente..."
         },
         {
             id:2,
             pathD:"M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16",
             spanText:"Borrar etiqueta",
-            textDet:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor in delectus fugit dolores quod"
+            textDet:"Presionando este boton puedes eliminar una etiqueta (plantilla) de la lista"
         },
         {
             id:3,
-            pathD:"M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16",
+            pathD:"M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z",
             spanText:"Imprimir etiqueta",
-            textDet:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor in delectus fugit dolores quod"
+            textDet:"Presionando este boton para seleccionar la plantilla que deseas imprimir en una etiqueta"
         }
     ]
     return (
